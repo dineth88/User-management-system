199f9b3d46468adfbcbc212ca03efeed2b21378a
dineth88 <dinethoshitha00@gmail.com>
2024-12-15 01:21:43 +0530

    Implemented create user functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,61 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([]);
+  const [username, setUsername] = useState([]);
+  const [password, setPassword] = useState([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/users/");
+      const data = await response.json();
+      setUsers(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const addUser = async()=>{
+      const userData = {
+        username,
+        password
+      };
+      try{
+      const response = await fetch("http://127.0.0.1:8000/api/users/create/", {
+        method:"POST",
+        headers:{
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+      });
+      const data = await response.json();
+      console.log(data);
+    }catch(err){
+        console.log(err);
+      }
+  };
 
   return (
     <>
-     <h1>ProfiLink</h1>
-     <div>
-        <input type='text' placeholder='Username'/>
-        <input type='password' placeholder='Password'/>
-        <button>Add User</button>
+      <h1>ProfiLink</h1>
+      <div>
+        <input type="text" placeholder="Username" onChange={(e)=>setUsername(e.target.value)}/>
+        <input type="password" placeholder="Password"  onChange={(e)=>setPassword(e.target.value)}/>
+        <button onClick={addUser}>Add User</button>
       </div>
+      {users.map((user) => (
+        <div>
+          <p>Username: {user.username}</p>
+          <p>Password: {user.password}</p>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
